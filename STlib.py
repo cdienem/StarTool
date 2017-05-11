13e9f94856fe2efd102ed4191a557bf0ddbbf95f
Chris Dienemann <cdienem@gwdg.de>
2017-05-11 14:14:32 +0200

    Implement stable --select_star

--- STlib.py
+++ STlib.py
@@ -258,22 +258,24 @@ class StarTool:
 		self.QUERY.append("SELECT * FROM ? WHERE \""+col+"\" REGEXP '"+pattern+"'")
 
 	def select_star(self, starfile, opts):
-	# 
-		# retrieve the tablename of the reference star file
+		# Starfile should be pre-loaded with star2db()
+		# Retrieve the tablename of the reference star file
 		reftable = self.STARTABLES[starfile][0]
-		# go through the options
-		q = "SELECT * FROM ? AS t1 WHERE 1"
+		# Prepare query
+		q = "SELECT * FROM ? AS t1 WHERE EXISTS (SELECT * FROM "+reftable+" AS t2 WHERE 1"
+		# Go through the options
 		for option in opts:
 			# Exclude the ignored ones
 			if option != None:
 				if option[1] != 0:
 					#Testing existensce of such parameters in the other table
-					q += " AND EXISTS (SELECT * FROM "+reftable+" AS t2 WHERE t2."+option[0]+" BETWEEN t1."+option[0]+"-"+str(option[1])+" AND t1."+option[0]+"+"+str(option[1])+")" 
-					print q
+					q += " AND t2."+option[0]+" BETWEEN t1."+option[0]+"-"+str(option[1])+" AND t1."+option[0]+"+"+str(option[1])+"" 
 				else:
-					q += " AND EXISTS (SELECT )"
-# SELECT * FROM table WHERE 				
-		pass
+					q += " AND t2."+option[0]+"=t1."+option[0]+""
+		# Add closing bracket
+		q+=")"
+		# Append to selector
+		self.QUERY.append(q)
 
 	def getTableNum(self, starfile):
 		return len(self.STARTABLES[starfile])
