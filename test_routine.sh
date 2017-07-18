46afcf18cb0e8eec41e417bcd358577d6c64ebd0
Chris Dienemann <cdienem@gwdg.de>
2017-07-18 16:13:55 +0200

    Extend unit tests

--- test_routine.sh
+++ test_routine.sh
@@ -3,11 +3,11 @@
 # this will be a test routine script to test basal functionality of the startool
 # it only tests for certain expected outputs
 
-echo "Starting StarTool test..."
+echo "Starting StarTool unit test..."
 
 # load std file and test --debug first
 
-if $(python startool.py testfiles/test_startup.star --debug | grep -q "Query"); then
+if $(python startool.py testfiles/2test_startup.star --debug | grep -q "Query"); then
 	echo "CHECK OK --debug"
 else
 	echo "CHECK FAILED --debug"
@@ -19,7 +19,7 @@ fi
 
 
 #--info
-if $(python startool.py testfiles/test_startup.star --info | grep -q "_rlnAngleRot"); then
+if $(python startool.py testfiles/2test_startup.star --info | grep -q "_rlnAngleRot"); then
 	echo "CHECK OK --info"
 else
 	echo "CHECK FAILED --info"
@@ -27,7 +27,7 @@ else
 fi
 
 #--show
-if $(python startool.py testfiles/test_startup.star --show | grep -q "ref_projections.mrcs"); then
+if $(python startool.py testfiles/2test_startup.star --show | grep -q "ref_projections.mrcs"); then
 	echo "CHECK OK --show"
 else
 	echo "CHECK FAILED --show"
@@ -35,7 +35,7 @@ else
 fi
 
 #--select
-if $(python startool.py testfiles/test_startup.star --select _rlnAngleRot=45 --show | grep -q "45.0"); then
+if $(python startool.py testfiles/2test_startup.star --select _rlnAngleRot=45 --show | grep -q "45.0"); then
 	echo "CHECK OK --select"
 else
 	echo "CHECK FAILED --select"
@@ -46,7 +46,7 @@ fi
 if [ -f testfiles/tmp.star ]; then
 	rm -f testfiles/tmp.star
 fi
-python startool.py testfiles/test_startup.star --select _rlnAngleRot=45 --write_selection testfiles/tmp.star > /dev/null
+python startool.py testfiles/2test_startup.star --select _rlnAngleRot=45 --write_selection testfiles/tmp.star > /dev/null
 if [ -f testfiles/tmp.star ]; then
 	echo "CHECK OK --write_selection"
 	rm -f testfiles/tmp.star
@@ -58,7 +58,7 @@ fi
 if [ -f testfiles/tmp.star ]; then
 	rm -f testfiles/tmp.star
 fi
-python startool.py testfiles/test_startup.star --write testfiles/tmp.star > /dev/null
+python startool.py testfiles/2test_startup.star --write testfiles/tmp.star > /dev/null
 if [ -f testfiles/tmp.star ]; then
 	echo "CHECK OK --write"
 	rm -f testfiles/tmp.star
@@ -68,38 +68,68 @@ fi
 
 
 #--use USE
-if $(python startool.py testfiles/test_twotable.star --use test_twotable_data_two --debug | grep -q "CURRENT: test_twotable_data_two"); then
+if $(python startool.py testfiles/2test_twotable.star --use 2test_twotable_data_two --debug | grep -q "CURRENT: 2test_twotable_data_two"); then
 	echo "CHECK OK --use"
 else
 	echo "CHECK FAILED --use"
 fi
 
-
 #--select_regex
 
-#--select_star
+if $(python startool.py testfiles/2test_startup.star --select_regex _rlnImageName='1\d{1}@r' --show | grep -q "11@ref"); then
+	echo "CHECK OK --select_regex"
+else
+	echo "CHECK FAILED --select_regex"
+fi
 
-#--select_fancy
+#--select_star
+if $(python startool.py testfiles/2test_startup.star --select_star testfiles/2test_reference.star:_rlnAngleRot --show | grep -q "45.0"); then
+	echo "CHECK OK --select_star (no variation)"
+else
+	echo "CHECK FAILED --select_star (no variation)"
+fi
+
+if $(python startool.py testfiles/2test_startup.star --select_star testfiles/2test_reference.star:_rlnAngleRot[2] --show | grep -q "47.0"); then
+	echo "CHECK OK --select_star (variation)"
+else
+	echo "CHECK FAILED --select_star (variation)"
+fi
+
 
 #--release -> check output of debug
 
+if $(python startool.py testfiles/2test_startup.star --release --debug | grep -q "CURRENT: NONE"); then
+	echo "CHECK OK --release"
+else
+	echo "CHECK FAILED --release"
+fi
+
+
 #--deselect -> check output of debug
 
-#--sort
-
-#--tros
+if $(python startool.py testfiles/2test_startup.star --select _rlnAngleRot=* --deselect --debug | grep -q "Query: SELECT ROWID,\* FROM \""); then
+	echo "CHECK OK --deselect"
+else
+	echo "CHECK FAILED --deselect"
+fi
 
 #--subset
 
-#--add_col
+#--sort -> check with subset
 
-#--delete_col
+#--tros -> check with subset
 
-#--rename_col
+#--add_col -> check info
 
-#--delete_table
+#--delete_col -> check info
 
-#--rename_table
+#--rename_col -> check info
+
+#--delete_table -> check info
+
+#--rename_table -> check info
+
+
 
 #--replace
 
@@ -111,4 +141,4 @@ fi
 
 #--merge
 
-#--query
+#--math
